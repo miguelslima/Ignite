3d76fd09f27488df9ecfb6a0e49b26d97c12d8eb
Miguel <miguelsousalima@hotmail.com>
2021-03-31 22:24:22 -0300

    Modulo 3 - Back end no front - Aula 1

--- FrontEnd/ignews/src/components/SignInButton/index.tsx
+++ FrontEnd/ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Miguel Lima
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        onClick={() => signOut()}
+        className={styles.closeIcon}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      onClick={() => signIn("github")}
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
