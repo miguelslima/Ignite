6f30f23dd4f854f9974ca0c1149c11a1176af1a0
Miguel <miguelsousalima@hotmail.com>
2021-03-31 22:58:55 -0300

    Modulo 3 - Usando FAUBADB - Aula 2

--- FrontEnd/ignews/src/components/SignInButton/index.tsx
+++ FrontEnd/ignews/src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
