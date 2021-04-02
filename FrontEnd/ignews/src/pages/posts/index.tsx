b25c87fcbf3e344a4ebdda44e6ecb47ccae204f2
Miguel <miguelsousalima@hotmail.com>
2021-04-02 14:18:41 -0300

    Modulo 3 - Aula CMS - Aula 1

--- FrontEnd/ignews/src/pages/posts/index.tsx
+++ FrontEnd/ignews/src/pages/posts/index.tsx
@@ -1,8 +1,23 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -11,35 +26,51 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>02 de abril de 2021</time>
-            <strong>Creating by Miguel Lima</strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
-          <a>
-            <time>02 de abril de 2021</time>
-            <strong>Creating by Miguel Lima</strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
-          <a>
-            <time>02 de abril de 2021</time>
-            <strong>Creating by Miguel Lima</strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  // console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
