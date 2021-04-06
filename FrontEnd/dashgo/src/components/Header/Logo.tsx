7e3a888f9941b8e5925daa04e6f9ff9157105082
Miguel <miguelsousalima@hotmail.com>
2021-04-06 00:48:56 -0300

    FrontEnd - Cap 4 - Aula 4 - Responsividade no Chakra

--- FrontEnd/dashgo/src/components/Header/Logo.tsx
+++ FrontEnd/dashgo/src/components/Header/Logo.tsx
@@ -2,7 +2,12 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+    <Text
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
       dashGo
       <Text as="span" marginLeft="1" color="blue.500">
         .
