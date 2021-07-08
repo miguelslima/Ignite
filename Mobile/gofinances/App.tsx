3f4613e770a916152c98115dbd45f2916d1c01c3
Miguel <miguelsousalima@hotmail.com>
2021-07-07 23:57:16 -0300

    GoFinances - Add styles-components

--- Mobile/gofinances/App.tsx
+++ Mobile/gofinances/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <Dashboard />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
