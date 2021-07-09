3d71f377a9a05ad177b0b64988ebab2daa0a702d
Miguel <miguelsousalima@hotmail.com>
2021-07-09 00:00:13 -0300

    GoFinances - props styled components

--- Mobile/gofinances/src/screens/Dashboard/styles.ts
+++ Mobile/gofinances/src/screens/Dashboard/styles.ts
@@ -59,5 +59,11 @@ export const UserName = styled.Text`
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secundary};
-  font-size:  ${RFValue(24)}px;;
+  font-size: ${RFValue(24)}px; ;
 `;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})``;
