a07c2c70531ccc0703e55e83ee0ab5b74575a3c1
Miguel <miguelsousalima@hotmail.com>
2021-07-17 18:04:43 -0300

    RentX - Home and component Car

--- Mobile/rentx/src/screens/Home/index.tsx
+++ Mobile/rentx/src/screens/Home/index.tsx
@@ -1,16 +1,42 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
+import { RFValue } from "react-native-responsive-fontsize";
+import { StatusBar } from "react-native";
 
-import { Container } from "./styles";
+import Logo from "../../assets/logo.svg";
 
-interface HomeProps {
-  children: ReactNode;
-}
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import Car from "../../components/Car";
 
-export function Home({ children }: HomeProps) {
+export function Home() {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120,
+    },
+    thumbnail:
+      "https://w7.pngwing.com/pngs/853/38/png-transparent-2017-audi-r8-car-audi-rs5-audi-r8-lms-2016-audi-sedan-car-performance-car.png",
+  };
+
   return (
     <Container>
-      <h1>Home</h1>
-      {children}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carData} />
+      <Car data={carData} />
+      <Car data={carData} />
     </Container>
   );
 }
