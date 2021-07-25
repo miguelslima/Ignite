d13888017e32dee327882d3a27c97d6cff16cb3d
Miguel <miguelsousalima@hotmail.com>
2021-07-25 18:55:09 -0300

    RentX - Botao flutuante

--- Mobile/rentx/src/screens/Home/index.tsx
+++ Mobile/rentx/src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 import { StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import Logo from '../../assets/logo.svg';
 
-import { CarList, Container, Header, HeaderContent, TotalCars } from './styles';
+import {
+  CarList,
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  MyCarsButton,
+} from './styles';
 import { Car } from '../../components/Car';
 import api from '../../services/api';
 
@@ -13,12 +22,17 @@ import { CarDTO } from '../../dtos/CarDTO';
 import { Loading } from '../../components/Loading';
 
 export function Home() {
+  const theme = useTheme();
   const navigation = useNavigation();
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
+  }
+
+  function handleOpenMyCars() {
+    navigation.navigate('MyCars');
   }
 
   useEffect(() => {
@@ -62,6 +76,10 @@ export function Home() {
           )}
         />
       )}
+
+      <MyCarsButton onPress={handleOpenMyCars}>
+        <Ionicons name='ios-car-sport' size={24} color={theme.colors.shape} />
+      </MyCarsButton>
     </Container>
   );
 }
---
+++ Mobile/rentx/src/screens/MyCars/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+export function MyCars() {
+  return (
+    <Container>
+      <Text>MyCars</Text>
+    </Container>
+  );
+}
