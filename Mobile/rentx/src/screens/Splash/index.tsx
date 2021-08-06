29d1faded13100bb482a2b2d7a550d98f563df44
Miguel <miguelsousalima@hotmail.com>
2021-08-05 23:02:08 -0300

    Rentx - Splash animated

--- Mobile/rentx/src/screens/Splash/index.tsx
+++ Mobile/rentx/src/screens/Splash/index.tsx
@@ -1,15 +1,85 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import Animated from 'react-native-reanimated';
-import { Button } from 'react-native';
+import { useEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  interpolate,
+  Extrapolate,
+  runOnJS,
+} from 'react-native-reanimated';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
 export function Splash() {
+  const splashAnimation = useSharedValue(0);
+  const navigation = useNavigation();
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 50],
+        [1, 0],
+        Extrapolate.CLAMP
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+  const LogoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [0, 0.3, 1],
+        Extrapolate.CLAMP
+      ),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  function startApp() {
+    navigation.navigate('Home');
+  }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 1000 }, () => {
+      'worklet';
+      runOnJS(startApp)();
+    });
+  });
+
   return (
     <Container>
-      <Animated.View />
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
 
-      <Button title='Mover' onPress={() => {}} />
+      <Animated.View style={[LogoStyle, { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   );
 }
