557641dae185d764fbad9ed445c7029b85b4d04c
Miguel <miguelsousalima@hotmail.com>
2021-08-23 22:33:32 -0300

    RentX - Acessibilidade form

--- Mobile/rentx/src/screens/SignIn/index.tsx
+++ Mobile/rentx/src/screens/SignIn/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { StatusBar, Text } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StatusBar,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -10,48 +15,52 @@ import { Container, Header, SubTitle, Title, Form, Footer } from './styles';
 
 export function SignIn() {
   return (
-    <Container>
-      <StatusBar
-        barStyle='dark-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <Header>
-        <Title>Estamos {'\n'}quase lá.</Title>
-        <SubTitle>
-          Faça seu login para começar {'\n'}uma experiência incrível.
-        </SubTitle>
-      </Header>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle='dark-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          <Header>
+            <Title>Estamos {'\n'}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar {'\n'}uma experiência incrível.
+            </SubTitle>
+          </Header>
 
-      <Form>
-        <Input
-          iconName='mail'
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
+          <Form>
+            <Input
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
 
-        <PasswordInput iconName='lock' placeholder='Senha' />
-      </Form>
+            <PasswordInput iconName='lock' placeholder='Senha' />
+          </Form>
 
-      <Footer>
-        <Button
-          title='Login'
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
+          <Footer>
+            <Button
+              title='Login'
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
 
-        <Button
-          title='Criar conta gratuita'
-          onPress={() => {}}
-          enabled={true}
-          loading={false}
-          color={theme.colors.background_secondary}
-          light={true}
-        />
-      </Footer>
-    </Container>
+            <Button
+              title='Criar conta gratuita'
+              onPress={() => {}}
+              enabled={true}
+              loading={false}
+              color={theme.colors.background_secondary}
+              light={true}
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
