8fc92d69e56571b932af6f2c01dde03cb0a2ecf9
Miguel <miguelsousalima@hotmail.com>
2021-08-31 23:34:48 -0300

    RentX - Routes bottons tabs

--- Mobile/rentx/src/routes/app.tab.routes.tsx
+++ Mobile/rentx/src/routes/app.tab.routes.tsx
@@ -1,19 +1,62 @@
 import React from 'react';
+import { Platform } from 'react-native';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
 
 import { AppStackRoutes } from './app.stack.routes';
 
-import { Home } from '../screens/Home';
+import HomeSvg from '../assets/home.svg';
+import CarSvg from '../assets/car.svg';
+import PeopleSvg from '../assets/people.svg';
+
 import { MyCars } from '../screens/MyCars';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator headerMode='none' initialRouteName='SignIn'>
-      <Screen name='Home' component={AppStackRoutes} />
-      <Screen name='MyCars' component={MyCars} />
-      <Screen name='Profile' component={Home} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.main,
+        inactiveTintColor: theme.colors.text_detail,
+        showLabel: false,
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary,
+        },
+      }}
+    >
+      <Screen
+        name='Home'
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name='MyCars'
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name='Profile'
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
