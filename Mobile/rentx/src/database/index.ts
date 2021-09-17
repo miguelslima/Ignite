623433e1ddbc6e0fd5d9969d8cbd0723c46dd869
Miguel <miguelsousalima@hotmail.com>
2021-09-16 23:45:38 -0300

    RentX - Tabela de carros

--- Mobile/rentx/src/database/index.ts
+++ Mobile/rentx/src/database/index.ts
@@ -1,8 +1,9 @@
-import { Database } from '@nozbe/watermelondb';
-import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
 
-import { schemas } from './schema';
-import { User } from './models/User';
+import { schemas } from "./schema";
+import { User } from "./models/User";
+import { Car } from "./models/Car";
 
 const adapter = new SQLiteAdapter({
   schema: schemas,
@@ -10,6 +11,6 @@ const adapter = new SQLiteAdapter({
 
 export const database = new Database({
   adapter,
-  modelClasses: [User],
+  modelClasses: [User, Car],
   actionsEnabled: true,
 });
